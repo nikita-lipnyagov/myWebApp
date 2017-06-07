93d7e2241d8d38abaf68a6ea26e13b38758ab202
Nikita Lipnyagov <lipnyagov@dlit.dp.ua>
2017-06-07 15:53:57 +0300

    04 servlet 3 0 1   annotated servlet

--- src/main/java/org/itstep/mywebapp/UserServlet.java
+++ src/main/java/org/itstep/mywebapp/UserServlet.java
@@ -1,6 +1,7 @@
 package org.itstep.mywebapp;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,6 +9,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
+@WebServlet("/users")
 public class UserServlet extends HttpServlet {
 
     private UserRepository repository = new UserRepository();
